8428427aec0c4b32c71009b7ecfdb468c26b1202
Naufra1 <gustavoresende3e@gmail.com>
2023-03-18 19:46:59 -0300

    Back-En

---
+++ Back-End/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { userRoute } from "./router/user.js";
+import { admRoute } from "./router/adm.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+//Rotas
+userRoute(app);
+admRoute(app);
+
+export default app;
