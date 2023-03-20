913dd23edb4e6656c15ccdad6c2621d09109d4f0
Naufra1 <gustavoresende3e@gmail.com>
2023-03-20 02:22:49 -0300

    Loading

--- Front-End/src/components/Layout/Loading.jsx
+++ Front-End/src/components/Layout/Loading.jsx
@@ -1,11 +1,11 @@
 import "./Loading.css";
 // import loading from "../../Img/loading.svg";
-const loading = require('../../img/loading.svg')
+const loading = required('../../img/loading.svg')
 
 function Loading() {
   return (
     <div className="loader">
-      <img className="loader__img" src={loading} alt="Loading" />
+      <img className="loader__img" src={loading.default} alt="Loading" />
     </div>
   );
 }
